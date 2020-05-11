444d45e49c477dc4689324e019bc208da5d6a98e
Azim Atefi <atefiazim1997@gmail.com>
2020-05-11 19:42:12 +0430

    Update CardStack.js

--- CardStack.js
+++ CardStack.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-import { polyfill } from 'react-lifecycles-compat';
-import {
-  View,
-  Animated,
-  PanResponder,
-  Dimensions,
-  Text,
-  Platform
-} from 'react-native';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {polyfill} from 'react-lifecycles-compat';
+import {Animated, Dimensions, PanResponder, Platform, StyleSheet, Text, View,} from 'react-native';
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get('window');
 
 class CardStack extends Component {
-
+  
   static distance(x, y) {
     return Math.hypot(x, y);
   }
-
+  
   constructor(props) {
     super(props);
     this.state = {
-      drag: new Animated.ValueXY({ x: 0, y: 0 }),
+      drag: new Animated.ValueXY({x: 0, y: 0}),
       dragDistance: new Animated.Value(0),
       sindex: 0, // index to the next card to be renderd mod card.length
       cardA: null,
@@ -37,33 +30,36 @@ class CardStack extends Component {
       onMoveShouldSetPanResponder: (evt, gestureState) => {
         const isVerticalSwipe = Math.sqrt(
           Math.pow(gestureState.dx, 2) < Math.pow(gestureState.dy, 2)
-        )
+        );
         if (!this.props.verticalSwipe && isVerticalSwipe) {
-          return false
+          return false;
         }
-        return Math.sqrt(Math.pow(gestureState.dx, 2) + Math.pow(gestureState.dy, 2)) > 10
+        return Math.sqrt(Math.pow(gestureState.dx, 2) + Math.pow(gestureState.dy, 2)) > 10;
       },
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
         const isVerticalSwipe = Math.sqrt(
           Math.pow(gestureState.dx, 2) < Math.pow(gestureState.dy, 2)
-        )
+        );
         if (!this.props.verticalSwipe && isVerticalSwipe) {
-          return false
+          return false;
         }
-        return Math.sqrt(Math.pow(gestureState.dx, 2) + Math.pow(gestureState.dy, 2)) > 10
+        return Math.sqrt(Math.pow(gestureState.dx, 2) + Math.pow(gestureState.dy, 2)) > 10;
       },
       onPanResponderGrant: (evt, gestureState) => {
         this.props.onSwipeStart();
-        this.setState({ touchStart: new Date().getTime() });
+        this.setState({touchStart: new Date().getTime()});
       },
       onPanResponderMove: (evt, gestureState) => {
         const movedX = gestureState.moveX - gestureState.x0;
         const movedY = gestureState.moveY - gestureState.y0;
         this.props.onSwipe(movedX, movedY);
-        const { verticalSwipe, horizontalSwipe } = this.props;
+        const {verticalSwipe, horizontalSwipe} = this.props;
         const dragDistance = this.distance((horizontalSwipe) ? gestureState.dx : 0, (verticalSwipe) ? gestureState.dy : 0);
         this.state.dragDistance.setValue(dragDistance);
-        this.state.drag.setValue({ x: (horizontalSwipe) ? gestureState.dx : 0, y: (verticalSwipe) ? gestureState.dy : 0 });
+        this.state.drag.setValue({
+          x: (horizontalSwipe) ? gestureState.dx : 0,
+          y: (verticalSwipe) ? gestureState.dy : 0
+        });
       },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
@@ -78,40 +74,35 @@ class CardStack extends Component {
           disableRightSwipe,
           disableBottomSwipe,
         } = this.props;
-
+        
         if (((Math.abs(gestureState.dx) > horizontalThreshold) ||
           (Math.abs(gestureState.dx) > horizontalThreshold * 0.6 &&
             swipeDuration < 150)
         ) && this.props.horizontalSwipe) {
-
+          
           const swipeDirection = (gestureState.dx < 0) ? width * -1.5 : width * 1.5;
           if (swipeDirection < 0 && !disableLeftSwipe) {
             this._nextCard('left', swipeDirection, gestureState.dy, this.props.duration);
-          }
-          else if (swipeDirection > 0 && !disableRightSwipe) {
+          } else if (swipeDirection > 0 && !disableRightSwipe) {
             this._nextCard('right', swipeDirection, gestureState.dy, this.props.duration);
-          }
-          else {
+          } else {
             this._resetCard();
           }
         } else if (((Math.abs(gestureState.dy) > verticalThreshold) ||
           (Math.abs(gestureState.dy) > verticalThreshold * 0.8 &&
             swipeDuration < 150)
         ) && this.props.verticalSwipe) {
-
+          
           const swipeDirection = (gestureState.dy < 0) ? height * -1 : height;
           if (swipeDirection < 0 && !disableTopSwipe) {
-
+            
             this._nextCard('top', gestureState.dx, swipeDirection, this.props.duration);
-          }
-          else if (swipeDirection > 0 && !disableBottomSwipe) {
+          } else if (swipeDirection > 0 && !disableBottomSwipe) {
             this._nextCard('bottom', gestureState.dx, swipeDirection, this.props.duration);
-          }
-          else {
+          } else {
             this._resetCard();
           }
-        }
-        else {
+        } else {
           this._resetCard();
         }
       },
@@ -122,7 +113,7 @@ class CardStack extends Component {
       },
     });
   }
-
+  
   componentDidUpdate(prevProps) {
     if (typeof this.props.children === 'undefined') return;
     if (!this._isSameChildren(this.props.children, prevProps.children)) {
@@ -140,35 +131,37 @@ class CardStack extends Component {
       });
     }
   }
-
-  _getIndex(index, cards){
-    return this.props.loop ? 
-      this.mod(index, cards):
+  
+  _getIndex(index, cards) {
+    return this.props.loop ?
+      this.mod(index, cards) :
       index;
   }
-
+  
   componentDidMount() {
     this.initDeck();
   }
-
+  
   _isSameChildren(a, b) {
     if (typeof a != typeof b) return false;
     if (typeof a === 'undefined') return false;
     if (Array.isArray(a) && Array.isArray(b)) {
       if (a.length != b.length) return false;
       for (let i in a) {
-        if (a[i].key != b[i].key) { return false }
+        if (a[i].key != b[i].key) {
+          return false;
+        }
       }
       return true;
     }
     if (a.key !== b.key) return false;
-
-    return true
+    
+    return true;
   }
-
+  
   initDeck() {
     if (typeof this.props.children === 'undefined') return;
-    const { children, loop } = this.props;
+    const {children, loop} = this.props;
     const cards = Array.isArray(children) ? children : [children];
     const initialIndexA = this.props.initialIndex < cards.length ? this.props.initialIndex : 0;
     const initialIndexB = loop ? this.mod(initialIndexA + 1, cards.length) : initialIndexA + 1;
@@ -181,7 +174,7 @@ class CardStack extends Component {
       sindex: initialIndexB + 1,
     });
   }
-
+  
   _resetCard() {
     Animated.timing(
       this.state.dragDistance,
@@ -193,79 +186,79 @@ class CardStack extends Component {
     Animated.spring(
       this.state.drag,
       {
-        toValue: { x: 0, y: 0 },
+        toValue: {x: 0, y: 0},
         duration: this.props.duration,
       }
     ).start();
   }
-
+  
   goBackFromTop() {
     this._goBack('top');
   }
-
+  
   goBackFromRight() {
     this._goBack('right');
   }
-
+  
   goBackFromLeft() {
     this._goBack('left');
   }
-
+  
   goBackFromBottom() {
     this._goBack('bottom');
   }
-
+  
   mod(n, m) {
     return ((n % m) + m) % m;
   }
-
+  
   _goBack(direction) {
-    const { cards, sindex, topCard } = this.state;
-
+    const {cards, sindex, topCard} = this.state;
+    
     if ((sindex - 3) < 0 && !this.props.loop) return;
-
-    const previusCardIndex = this.mod(sindex - 3, cards.length)
+    
+    const previusCardIndex = this.mod(sindex - 3, cards.length);
     let update = {};
     if (topCard === 'cardA') {
       update = {
         ...update,
         cardB: cards[previusCardIndex]
-
-      }
+        
+      };
     } else {
       update = {
         ...update,
         cardA: cards[previusCardIndex],
-      }
+      };
     }
-
+    
     this.setState({
       ...update,
       topCard: (topCard === 'cardA') ? 'cardB' : 'cardA',
       sindex: sindex - 1
     }, () => {
-
+      
       switch (direction) {
         case 'top':
-          this.state.drag.setValue({ x: 0, y: -height });
+          this.state.drag.setValue({x: 0, y: -height});
           this.state.dragDistance.setValue(height);
           break;
         case 'left':
-          this.state.drag.setValue({ x: -width, y: 0 });
+          this.state.drag.setValue({x: -width, y: 0});
           this.state.dragDistance.setValue(width);
           break;
         case 'right':
-          this.state.drag.setValue({ x: width, y: 0 });
+          this.state.drag.setValue({x: width, y: 0});
           this.state.dragDistance.setValue(width);
           break;
         case 'bottom':
-          this.state.drag.setValue({ x: 0, y: height });
+          this.state.drag.setValue({x: 0, y: height});
           this.state.dragDistance.setValue(width);
           break;
         default:
-
+        
       }
-
+      
       Animated.spring(
         this.state.dragDistance,
         {
@@ -273,47 +266,47 @@ class CardStack extends Component {
           duration: this.props.duration,
         }
       ).start();
-
+      
       Animated.spring(
         this.state.drag,
         {
-          toValue: { x: 0, y: 0 },
+          toValue: {x: 0, y: 0},
           duration: this.props.duration,
         }
       ).start();
-    })
+    });
   }
-
+  
   swipeTop(d = null) {
     this._nextCard('top', 0, -height, d || this.props.duration);
   }
-
+  
   swipeBottom(d = null) {
     this._nextCard('bottom', 0, height, d || this.props.duration);
   }
-
+  
   swipeRight(d = null) {
     this._nextCard('right', width * 1.5, 0, d || this.props.duration);
   }
-
+  
   swipeLeft(d = null) {
     this._nextCard('left', -width * 1.5, 0, d || this.props.duration);
   }
-
+  
   _nextCard(direction, x, y, duration = 400) {
-    const { verticalSwipe, horizontalSwipe, loop } = this.props;
-    const { sindex, cards, topCard } = this.state;
-
+    const {verticalSwipe, horizontalSwipe, loop} = this.props;
+    const {sindex, cards, topCard} = this.state;
+    
     // index for the next card to be renderd
     const nextCard = (loop) ? (Math.abs(sindex) % cards.length) : sindex;
-
+    
     // index of the swiped card
     const index = (loop) ? this.mod(nextCard - 2, cards.length) : nextCard - 2;
-
+    
     if (index === cards.length - 1) {
       this.props.onSwipedAll();
     }
-
+    
     if ((sindex - 2 < cards.length) || (loop)) {
       Animated.spring(
         this.state.dragDistance,
@@ -322,17 +315,17 @@ class CardStack extends Component {
           duration,
         }
       ).start();
-
+      
       Animated.timing(
         this.state.drag,
         {
-          toValue: { x: (horizontalSwipe) ? x : 0, y: (verticalSwipe) ? y : 0 },
+          toValue: {x: (horizontalSwipe) ? x : 0, y: (verticalSwipe) ? y : 0},
           duration,
         }
       ).start(() => {
-
+        
         const newTopCard = (topCard === 'cardA') ? 'cardB' : 'cardA';
-
+        
         let update = {};
         if (newTopCard === 'cardA') {
           update = {
@@ -346,14 +339,14 @@ class CardStack extends Component {
             cardA: cards[nextCard],
           };
         }
-        this.state.drag.setValue({ x: 0, y: 0 });
+        this.state.drag.setValue({x: 0, y: 0});
         this.state.dragDistance.setValue(0);
         this.setState({
           ...update,
           topCard: newTopCard,
           sindex: nextCard + 1
         });
-
+        
         this.props.onSwiped(index);
         switch (direction) {
           case 'left':
@@ -379,24 +372,24 @@ class CardStack extends Component {
           default:
         }
       });
-
+      
     }
   }
-
-
+  
+  
   /**
    * @description CardB’s click feature is trigger the CardA on the card stack. (Solved on Android)
    * @see https://facebook.github.io/react-native/docs/view#pointerevents
    */
   _setPointerEvents(topCard, topCardName) {
-    return { pointerEvents: topCard === topCardName ? "auto" : "none" }
+    return {pointerEvents: topCard === topCardName ? "auto" : "none"};
   }
-
+  
   render() {
-
-    const { secondCardZoom, renderNoMoreCards } = this.props;
-    const { drag, dragDistance, cardA, cardB, topCard, sindex } = this.state;
-
+    
+    const {secondCardZoom, renderNoMoreCards} = this.props;
+    const {drag, dragDistance, cardA, cardB, topCard, sindex} = this.state;
+    
     const scale = dragDistance.interpolate({
       inputRange: [0, 10, 220],
       outputRange: [secondCardZoom, secondCardZoom, 1],
@@ -407,12 +400,23 @@ class CardStack extends Component {
       outputRange: this.props.outputRotationRange,
       extrapolate: 'clamp',
     });
-
+    
+    const likeOpacity = drag.x.interpolate({
+      inputRange: [0, width / 4],
+      outputRange: [0, 1],
+    });
+    const nopeOpacity = drag.x.interpolate({
+      inputRange: [-width / 4, 0],
+      outputRange: [1, 0],
+    });
+    
+    const HotLabel = this.props.hotLabel;
+    const NotLabel = this.props.notLabel;
     return (
-      <View {...this._panResponder.panHandlers} style={[{ position: 'relative' }, this.props.style]}>
-
+      <View {...this._panResponder.panHandlers} style={[{position: 'relative'}, this.props.style]}>
+        
         {renderNoMoreCards()}
-
+        
         <Animated.View
           {...this._setPointerEvents(topCard, 'cardB')}
           style={[{
@@ -424,13 +428,28 @@ class CardStack extends Component {
               }
             }),
             transform: [
-              { rotate: (topCard === 'cardB') ? rotate : '0deg' },
-              { translateX: (topCard === 'cardB') ? drag.x : 0 },
-              { translateY: (topCard === 'cardB') ? drag.y : 0 },
-              { scale: (topCard === 'cardB') ? 1 : scale },
+              {rotate: (topCard === 'cardB') ? rotate : '0deg'},
+              {translateX: (topCard === 'cardB') ? drag.x : 0},
+              {translateY: (topCard === 'cardB') ? drag.y : 0},
+              {scale: (topCard === 'cardB') ? 1 : scale},
             ]
           }, this.props.cardContainerStyle]}>
           {cardB}
+          <View style={[StyleSheet.absoluteFill,
+            {
+              margin: 25,
+              zIndex: 2000,
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }]}
+          >
+            {this.props.notLabel && (
+              <NotLabel nopeOpacity={(topCard === 'cardB') ? nopeOpacity : 0} />
+            )}
+            {this.props.hotLabel && (
+              <HotLabel likeOpacity={(topCard === 'cardB') ? likeOpacity : 0} />
+            )}
+          </View>
         </Animated.View>
         <Animated.View
           {...this._setPointerEvents(topCard, 'cardA')}
@@ -443,24 +462,39 @@ class CardStack extends Component {
               }
             }),
             transform: [
-              { rotate: (topCard === 'cardA') ? rotate : '0deg' },
-              { translateX: (topCard === 'cardA') ? drag.x : 0 },
-              { translateY: (topCard === 'cardA') ? drag.y : 0 },
-              { scale: (topCard === 'cardA') ? 1 : scale },
+              {rotate: (topCard === 'cardA') ? rotate : '0deg'},
+              {translateX: (topCard === 'cardA') ? drag.x : 0},
+              {translateY: (topCard === 'cardA') ? drag.y : 0},
+              {scale: (topCard === 'cardA') ? 1 : scale},
             ]
           }, this.props.cardContainerStyle]}>
           {cardA}
+          <View style={[StyleSheet.absoluteFill,
+            {
+              margin: 25,
+              zIndex: 2000,
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }]}
+          >
+            {this.props.notLabel && (
+              <NotLabel nopeOpacity={(topCard === 'cardA') ? nopeOpacity : 0} />
+            )}
+            {this.props.hotLabel && (
+              <HotLabel likeOpacity={(topCard === 'cardA') ? likeOpacity : 0} />
+            )}
+          </View>
         </Animated.View>
-
       </View>
     );
   }
 }
 
 CardStack.propTypes = {
-
+  hotLabel: PropTypes.object,
+  notLabel: PropTypes.object,
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
-
+  
   style: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
   cardContainerStyle: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
   secondCardZoom: PropTypes.number,
@@ -477,38 +511,48 @@ CardStack.propTypes = {
   onSwiped: PropTypes.func,
   onSwipedAll: PropTypes.func,
   onSwipe: PropTypes.func,
-
+  
   disableBottomSwipe: PropTypes.bool,
   disableLeftSwipe: PropTypes.bool,
   disableRightSwipe: PropTypes.bool,
   disableTopSwipe: PropTypes.bool,
   verticalSwipe: PropTypes.bool,
   verticalThreshold: PropTypes.number,
-
+  
   horizontalSwipe: PropTypes.bool,
   horizontalThreshold: PropTypes.number,
   outputRotationRange: PropTypes.array,
   duration: PropTypes.number
-}
+};
 
 CardStack.defaultProps = {
-
+  hotLabel: false,
+  notLabel: false,
   style: {},
   cardContainerStyle: {},
   secondCardZoom: 0.95,
   loop: false,
   initialIndex: 0,
-  renderNoMoreCards: () => { return (<Text>No More Cards</Text>) },
+  renderNoMoreCards: () => {
+    return (<Text>No More Cards</Text>);
+  },
   onSwipeStart: () => null,
   onSwipeEnd: () => null,
-  onSwiped: () => { },
-  onSwipedLeft: () => { },
-  onSwipedRight: () => { },
-  onSwipedTop: () => { },
-  onSwipedBottom: () => { },
-  onSwipedAll: async () => { },
-  onSwipe: () => { },
-
+  onSwiped: () => {
+  },
+  onSwipedLeft: () => {
+  },
+  onSwipedRight: () => {
+  },
+  onSwipedTop: () => {
+  },
+  onSwipedBottom: () => {
+  },
+  onSwipedAll: async () => {
+  },
+  onSwipe: () => {
+  },
+  
   disableBottomSwipe: false,
   disableLeftSwipe: false,
   disableRightSwipe: false,
@@ -519,6 +563,6 @@ CardStack.defaultProps = {
   horizontalThreshold: width / 2,
   outputRotationRange: ['-15deg', '0deg', '15deg'],
   duration: 300
-}
+};
 polyfill(CardStack);
 export default CardStack;
